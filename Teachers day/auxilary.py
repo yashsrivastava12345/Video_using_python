505d2ecae803b180e796121af33434df00c5cbe4
Yash Srivastava <52215319+yashsrivastava12345@users.noreply.github.com>
2024-07-09 23:40:42 +0530

    Add files via upload

---
+++ Teachers day/auxilary.py
@@ -0,0 +1,16 @@
+xp=[105,255,405,555,705,865,1038,1175,1325,1475,1625,1775]
+xp1=[]
+yp2=26
+
+                    py.draw.ellipse(GD, (a1,b1,c1),(xp1[0],yp2,blueSquareVY,blueSquareVX))
+                    py.draw.ellipse(GD, (a1,b1,c1),(xp1[1],yp2,blueSquareVY,blueSquareVX))
+                    py.draw.ellipse(GD, (a1,b1,c1),(xp1[2],yp2,blueSquareVY,blueSquareVX))
+                    py.draw.ellipse(GD, (a1,b1,c1),(xp1[3],yp2,blueSquareVY,blueSquareVX))
+                    py.draw.ellipse(GD, (a1,b1,c1),(xp1[4],yp2,blueSquareVY,blueSquareVX))
+                    py.draw.ellipse(GD, (a1,b1,c1),(xp1[5],yp2,blueSquareVY,blueSquareVX))
+                    py.draw.ellipse(GD, (a1,b1,c1),(xp1[6],yp2,blueSquareVY,blueSquareVX))
+                    py.draw.ellipse(GD, (a1,b1,c1),(xp1[7],yp2,blueSquareVY,blueSquareVX))
+                    py.draw.ellipse(GD, (a1,b1,c1),(xp1[8],yp2,blueSquareVY,blueSquareVX))
+                    py.draw.ellipse(GD, (a1,b1,c1),(xp1[9],yp2,blueSquareVY,blueSquareVX))
+                    py.draw.ellipse(GD, (a1,b1,c1),(xp1[10],yp2,blueSquareVY,blueSquareVX))
+                    py.draw.ellipse(GD, (a1,b1,c1),(xp1[11],yp2,blueSquareVY,blueSquareVX))
